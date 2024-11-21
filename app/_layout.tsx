54e558d1ddda3426bfb848630b3c44ee8c8db2ad
Mehebbet Farzaliyev <72205368+Rafael-lo@users.noreply.github.com>
2024-11-21 03:03:37 -0800

    Work on rouing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-
+import "../global.css";
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -29,8 +29,10 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack initialRouteName='index'>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="(auth)" />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Index = () => {
+  return (
+    <View>
+      <Text className='text-green-500'>Welcome</Text>
+      <View className='w-10 h-10 bg-red-500'></View>
+    </View>
+  )
+}
+
+export default Index;
